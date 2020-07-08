b90f3401facf368ae9b324ac22b6c40181ea98f3
JPLABRADOR <vanhagarsux@gmail.com>
2020-07-08 14:08:21 -0700

    Added my test to test file

--- tests/imdbTest.js
+++ tests/imdbTest.js
@@ -1,14 +1,23 @@
 var iObjects = {}
+var actorData = require('../pageAssets/backToTheFutureArray')
+
 
 module.exports ={
     beforeEach: browser =>{
         iObjects = browser.page.imdbObjects()
-        iObjects.navigate
+        iObjects.maximizeWindow()
+        iObjects.navigate()
     },
     afterEach: browser =>{
         browser.end
     },
     'Search Test': browser =>{
         iObjects.searchTest()
+    },
+    'Test - Verify Cast Section': browser => {
+        actorData.forEach(test => {
+            iObjects
+                .verifyPage(test)
+        })
     }
 }
