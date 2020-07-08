f2022d809042312e4d4e1d5925d17e6c4061f459
JPLABRADOR <vanhagarsux@gmail.com>
2020-07-08 14:11:18 -0700

    Added my own assets array

---
+++ pageAssets/backToTheFutureArray.js
@@ -0,0 +1,214 @@
+module.exports = [
+    {
+        actor: "@michaelJFox",
+        actorPageHeader: "@michaelJFoxPageHeader",
+        actorPageHeaderText: "Michael J. Fox",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@christopherLloyd",
+        actorPageHeader: "@christopherLloydPageHeader",
+        actorPageHeaderText: "Christopher Lloyd",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@leaThompson",
+        actorPageHeader: "@leaThompsonPageHeader",
+        actorPageHeaderText: "Lea Thompson",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@crispinGlover",
+        actorPageHeader: "@crispinGloverPageHeader",
+        actorPageHeaderText: "Crispin Glover",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@thomasFWilson",
+        actorPageHeader: "@thomasFWilsonPageHeader",
+        actorPageHeaderText: "Thomas F. Wilson",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@claudiaWells",
+        actorPageHeader: "@claudiaWellsPageHeader",
+        actorPageHeaderText: "Claudia Wells",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@marcMcClure",
+        actorPageHeader: "@marcMcClurePageHeader",
+        actorPageHeaderText: "Marc McClure",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@wendieJoSperber",
+        actorPageHeader: "@wendieJoSperberPageHeader",
+        actorPageHeaderText: "Wendie Jo Sperber",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@georgeDiCenzo",
+        actorPageHeader: "@georgeDiCenzoPageHeader",
+        actorPageHeaderText: "George DiCenzo",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@francesLeeMcCain",
+        actorPageHeader: "@francesLeeMcCainPageHeader",
+        actorPageHeaderText: "Frances Lee McCain",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@jamesTolkan",
+        actorPageHeader: "@jamesTolkanPageHeader",
+        actorPageHeaderText: "James Tolkan",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@jJCohen",
+        actorPageHeader: "@jJCohenPageHeader",
+        actorPageHeaderText: "J.J. Cohen",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@caseySiemaszko",
+        actorPageHeader: "@caseySiemaszkoPageHeader",
+        actorPageHeaderText: "Casey Siemaszko",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@billyZane",
+        actorPageHeader: "@billyZanePageHeader",
+        actorPageHeaderText: "Billy Zane",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@harryWatersJr",
+        actorPageHeader: "@harryWatersJrPageHeader",
+        actorPageHeaderText: "Harry Waters Jr.",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+
+    // Characters
+    {
+        actor: "@martyMcFly",
+        actorPageHeader: "@martyMcFlyPageHeader",
+        actorPageHeaderText: "Michael J. Fox: Marty McFly",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@drEmmettBrown",
+        actorPageHeader: "@drEmmettBrownPageHeader",
+        actorPageHeaderText: "Christopher Lloyd: Dr. Emmett Brown",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@lorraineBaines",
+        actorPageHeader: "@lorraineBainesPageHeader",
+        actorPageHeaderText: "Lea Thompson: Lorraine Baines",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@georgeMcFly",
+        actorPageHeader: "@georgeMcFlyPageHeader",
+        actorPageHeaderText: "Crispin Glover: George McFly",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@biffTannen",
+        actorPageHeader: "@biffTannenPageHeader",
+        actorPageHeaderText: "Thomas F. Wilson: Biff Tannen",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@jenniferParker",
+        actorPageHeader: "@jenniferParkerPageHeader",
+        actorPageHeaderText: "Claudia Wells: Jennifer Parker",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@daveMcFly",
+        actorPageHeader: "@daveMcFlyPageHeader",
+        actorPageHeaderText: "Marc McClure: Dave McFly",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@lindaMcFly",
+        actorPageHeader: "@lindaMcFlyPageHeader",
+        actorPageHeaderText: "Wendie Jo Sperber: Linda McFly",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@samBaines",
+        actorPageHeader: "@samBainesPageHeader",
+        actorPageHeaderText: "George DiCenzo: Sam Baines",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@stellaBaines",
+        actorPageHeader: "@stellaBainesPageHeader",
+        actorPageHeaderText: "Frances Lee McCain: Stella Baines",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@mrStrickland",
+        actorPageHeader: "@mrStricklandPageHeader",
+        actorPageHeaderText: "James Tolkan: Mr. Strickland",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@skinhead",
+        actorPageHeader: "@skinheadPageHeader",
+        actorPageHeaderText: "J.J. Cohen: Skinhead",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@threeD",
+        actorPageHeader: "@threeDPageHeader",
+        actorPageHeaderText: "Casey Siemaszko: 3-D",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@match",
+        actorPageHeader: "@matchPageHeader",
+        actorPageHeaderText: "Billy Zane: Match",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+    {
+        actor: "@marvinBerry",
+        actorPageHeader: "@marvinBerryPageHeader",
+        actorPageHeaderText: "Harry Waters Jr.: Marvin Berry",
+        poster: "@mainPagePoster",
+        posterTitle: "Back to the Future Poster"
+    },
+]
