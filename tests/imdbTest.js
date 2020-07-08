20d43fa4d27d298bc5dc32e449235e4f4dda91df
Stephanie Hansen <Steph@Stephanies-MacBook-Pro.local>
2020-07-08 16:05:19 -0600

    steph

--- tests/imdbTest.js
+++ tests/imdbTest.js
@@ -1,14 +1,27 @@
 var iObjects = {}
-
+var actorData = require('../pageAssets/backToTheFutureArray')
+ 
+ 
 module.exports ={
-    beforeEach: browser =>{
-        iObjects = browser.page.imdbObjects()
-        iObjects.navigate
-    },
-    afterEach: browser =>{
-        browser.end
-    },
-    'Search Test': browser =>{
-        iObjects.searchTest()
-    }
+   beforeEach: browser =>{
+       iObjects = browser.page.imdbObjects()
+       iObjects.maximizeWindow()
+       iObjects.navigate()
+   },
+   afterEach: browser =>{
+       browser.end
+   },
+   'Search Test': browser => {
+       iObjects.searchTest()
+   },
+   'Test - Verify Cast Section': browser => {
+       actorData.forEach(test => {
+           iObjects
+               .verifyPage(test)
+       })
+   },
+ 
+   'BTS Menu Test': browser => {
+       iObjects.btfMenuTest()
+   }
 }
