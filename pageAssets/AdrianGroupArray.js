d834c8379efa4e730422c23c024803490667e800
Adrian Richards <adrian.trilogy@gmail.com>
2020-07-08 09:36:18 -0600

    Adrians Stuff

---
+++ pageAssets/AdrianGroupArray.js
@@ -0,0 +1,16 @@
+module.exports = {
+    search1: 'Back to the Future',
+    verifyVideo1: 'https://www.imdb.com/video/vi2099035673?ref_=ttvi_vi_imdb_1',
+    verifyVideo2: 'https://www.imdb.com/video/vi3626548249?ref_=ttvi_vi_imdb_2',
+    verifyVideo3: 'https://www.imdb.com/video/vi474724633?ref_=ttvi_vi_imdb_5',
+    verifyVideo4: 'https://www.imdb.com/video/vi3662328601?ref_=ttvi_vi_imdb_6',
+    verifyVideo5: 'https://www.imdb.com/video/vi252380953?ref_=ttvi_vi_imdb_7',
+    verifyPG: 'Photo Gallery',
+    verifySF: 'Still Frame',
+    verifyE: 'Event',
+    verifyPoster: 'Poster',
+    verifyProduct: 'Product',
+    verifyPub: 'Publicity',
+    verifyBTS: 'Behind The Scenes',
+    verifyPA: 'Production Art'
+}
